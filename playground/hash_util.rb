2fd106bfeea1a2d1147b3898d3f6a93bf2860208
bastengao <bastengao@gmail.com>
2015-02-11 15:29:14 +0800

    add hash

---
+++ playground/hash_util.rb
@@ -0,0 +1,13 @@
+class HashUtil
+
+  # 返回 value 是最大数对应的 key
+  def key_of_max(hash)
+    # TODO
+  end
+
+  # 将 key 和 value 组成新的 hash
+  def to_hash(key_array, value_array)
+    # TODO
+  end
+
+end
--- playground/test.rb
+++ playground/test.rb
@@ -45,3 +45,21 @@ class StringUtilTest < MiniTest::Unit::TestCase
     assert_equal result, 'I am awesome'
   end
 end
+
+class HashUtilTest < MiniTest::Unit::TestCase
+  def setup
+    @hash = HashUtil.new
+  end
+
+  def test_key_of_max
+    result = @hash.key_of_max({'foo'=> 1234, 'bar'=> 4567, 'max_key' => 9999})
+    assert_equal result, 'max_key'
+  end
+  
+  def test_to_hash
+    key_array = ['string', :symbol, 456]
+    value_array = [1, 'abc', 654]
+    result = @hash.to_hash key_array, value_array
+    assert_equal result, {'string' => 1, :symbol => 'abc', 456 => 654}
+  end
+end
